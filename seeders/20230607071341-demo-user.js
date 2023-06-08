fcd49b45e91d81a2e15b1777e7a97133eedc3efb
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-08 10:48:23 +0530

    made changes in user seeder

--- seeders/20230607071341-demo-user.js
+++ seeders/20230607071341-demo-user.js
@@ -11,7 +11,7 @@ module.exports = {
       email: 'super_admin@gmail.com',
       password: bcrypt.hashSync('super_admin', 8),
       phoneNo: 8749892048,
-      isEmailVerified : true,
+      isEmailVerified : 1,
       role: constant.userType.super_admin,
       createdAt : new Date(),
       updatedAt : new Date()
