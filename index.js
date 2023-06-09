ef05ad30df5538e424d47066a089299e92a3cfcf
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-09 13:48:50 +0530

    added separate api for email verify

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-sequelize.sync()
-  .then(() => {
-    console.log('sync successfully');
-  })
-  .catch((err) => {
-    console.log('Error while sycing', err.message);
-  });
+// sequelize.sync()
+//   .then(() => {
+//     console.log('sync successfully');
+//   })
+//   .catch((err) => {
+//     console.log('Error while sycing', err.message);
+//   });
 
 
 
 const apiRoutes = require('./routes/apiRoutes')
 app.use(apiRoutes)
-
+// verifying email url: "http://localhost:7777/v1/customer/verify/email?token=<%= token %>
 app.listen(serverConfig.PORT, ()=>{
     console.log('Server is started on Port:',serverConfig.PORT)
 })
