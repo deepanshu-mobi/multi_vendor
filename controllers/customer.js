20be8a7c9505fbdff187b43c585cf2bfb2f350c7
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-09 16:37:13 +0530

    made changes in customer

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,58 +1,64 @@
-const customerService = require('../services/customerService')
-const { Customer } = require('../models')
-const { StatusCodes } = require('http-status-codes')
+const customerService = require('../services/customerService');
+const { Customer } = require('../models');
+const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
-const serverConfig = require('../config/server.config')
-const bcrypt = require('bcryptjs')
+const serverConfig = require('../config/server.config');
+const bcrypt = require('bcryptjs');
 
-exports.register = async(req, res) => {
-    
-    const customer = await customerService.createCustomer(req.body);
-    const response = {
-        customerId: customer.customerId,
-        name: customer.name,
-        token: customer.token
-    }
-    return res.status(StatusCodes.OK).send(response)
-}
+exports.register = async (req, res) => {
+  const customer = await customerService.createCustomer(req.body);
+  const response = {
+    customerId: customer.customerId,
+    name: customer.name,
+    token: customer.token,
+  };
+  return res.status(StatusCodes.OK).send(response);
+};
 
-
-exports.verifyEmail = async(req, res)=> {
+exports.verifyEmail = async (req, res) => {
 
     const { token } = req.query;
-    const customer = await Customer.findOne({ where: { token }})
-    if(customer){
-        jwt.verify(customer.token, serverConfig.SECRET, async(err, decoded) =>{
-            if(err){
-                return res.status(StatusCodes.UNAUTHORIZED).send({
-                    message: 'unauthorized'
-                })
-            }
-            customer.isEmailVerified = 1
-            await customer.save()
-            res.render('templates/emailVerify')
-        })
-    }
-}
+    let message = '';
+      jwt.verify(token, serverConfig.SECRET, async (err, decoded) => {
+        if (err) {
+          message = err.message
+          return res.status(StatusCodes.BAD_REQUEST).send({
+            message: err.message,
+          });
+        } 
+        const email = decoded.id
+        const customer = await Customer.findOne({ where: { email } });
+        customer.isEmailVerified = 1;
+        await customer.save();
+        res.render('templates/emailVerify', { message });
+      });
 
+};
 
 exports.login = async (req, res) => {
-
-    const { password } = req.body
-    const customer = await customerService.loginCustomer(req.body)
-    if(customer){
-        const isValidPassword = bcrypt.compare(password, customer.password)
-        if(isValidPassword){
-            if(customer.isEmailVerified === 0){
-                return res.status(StatusCodes.BAD_REQUEST).send({ mesg: 'Email is not verified yet try after sometime later'})
-            }
-            const response = {
-                customerId: customer.customerId,
-                name: customer.name,
-            }
-            return res.status(StatusCodes.OK).send({customer: response, mesg: 'Successfully loggedIn' })
-        }
-        return res.status(StatusCodes.BAD_REQUEST).send({ mesg: 'Email or Password may be wrong please try again' })
+  const { password } = req.body;
+  const customer = await customerService.loginCustomer(req.body);
+  if (customer) {
+    const isValidPassword = bcrypt.compare(password, customer.password);
+    if (isValidPassword) {
+      if (customer.isEmailVerified === 0) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .send({ mesg: 'Email is not verified yet try after sometime later' });
+      }
+      const response = {
+        customerId: customer.customerId,
+        name: customer.name,
+      };
+      return res
+        .status(StatusCodes.OK)
+        .send({ customer: response, mesg: 'Successfully loggedIn' });
     }
-    return res.status(StatusCodes.BAD_REQUEST).send({ mesg: 'User does not exist'})
-}
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send({ mesg: 'Email or Password may be wrong please try again' });
+  }
+  return res
+    .status(StatusCodes.BAD_REQUEST)
+    .send({ mesg: 'User does not exist' });
+};
