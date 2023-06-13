6a992aaae87c2227e99bd98bde0e89de3bc6ba3f
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-13 18:33:24 +0530

    validating user registration

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -1,4 +1,28 @@
 const { body } = require('express-validator')
+const constant = require('../utils/constant')
+
+const userRegisterValidator = [
+    body('name')
+    .notEmpty()
+    .withMessage('Name is required'),
+    body('email')
+    .notEmpty()
+    .withMessage('Email is required'),
+    body('password')
+    .notEmpty()
+    .withMessage('Password is required'),
+    body('role')
+    .notEmpty()
+    .withMessage('Role is required')
+    .custom( role => {
+        if(role == constant.userType.super_admin){
+            throw new Error('Only admin || vendor registration is allowed')
+        }
+        if(role != constant.userType.admin || role != constant.userType.vendor){
+            throw new Error('Not a valid role possible roles are admin || vendor')
+        }
+    })
+]
 
 
 const loginValidator = [
@@ -11,5 +35,6 @@ const loginValidator = [
 ]
 
 module.exports = {
+    userRegisterValidator,
     loginValidator,
 }
