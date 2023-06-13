294dce15a73b9442a4c6a945518b58df4f09088c
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-13 18:34:52 +0530

    added user registration route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const userController = require('../controllers/user')
 const { expressValidator, isAdmin } = require('../middleware/validator');
-const { loginValidator } = require('../validation/userValidation')
+const { loginValidator, userRegisterValidator } = require('../validation/userValidation')
 const { verifySession } = require('../middleware/verifySession')
 
+router.post('/user/register', userRegisterValidator, expressValidator, userController.register)
 router.post('/user/login', loginValidator, expressValidator, userController.login)
 router.use(verifySession)
 router.get('/user/customers', isAdmin, userController.findAll)
