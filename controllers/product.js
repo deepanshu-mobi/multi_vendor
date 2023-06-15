d446953575d42d8e493ae5432bebf1b66c9e3231
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-15 17:00:57 +0530

    made changes

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require('http-status-codes');
 
 exports.addProduct = async (req, res) => {
 
+    try{
     const { productName, description, price } = req.body
     const { email } = req.session.user 
     const body = {
@@ -18,4 +19,10 @@ exports.addProduct = async (req, res) => {
         productName: product.productName
     }
     return res.status(StatusCodes.CREATED).send(response)
+}catch(err){
+    console.log('Error while creating new product',err);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        mesg: 'Internal server error'
+    })
+}
 }
