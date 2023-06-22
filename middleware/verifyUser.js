9852ed31c7cb4425d1d152c3b2db99b73105393e
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-22 15:18:20 +0530

    made few changes

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -1,18 +1,17 @@
 const { User } = require('../models')
 const constant = require('../utils/constant')
 const { StatusCodes } = require('http-status-codes')
+const { response } = require('../utils/commonRes')
 
 const isVendor = async (req, res, next) => {
 
-    const { email } = req.session.user;
+    const { email } = req.email;
     const user = await User.findOne({ where: { email }});
     if(user.role == constant.userType.vendor && user.role == constant.userType.admin && user.role == constant.userType.super_admin){
         next()
     }
     else{
-        return res.status(StatusCodes.BAD_REQUEST).send({
-            mesg: 'Invalid access'
-        })
+        return res.status(StatusCodes.BAD_REQUEST).send(response.failed('Invalid access'))
     }
 }
 
