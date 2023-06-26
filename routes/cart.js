be8d70af023dc5524bf24f6348084e5f21740173
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-26 17:29:22 +0530

    added cart routes

---
+++ routes/cart.js
@@ -0,0 +1,10 @@
+const cartController = require('../controllers/cart');
+const router = require('express').Router();
+const { verifyToken } = require('../middleware/auth.jwt')
+
+router.use(verifyToken)
+router.post('/customer/cart', cartController.addToCart);
+router.delete('/customer/clear_cart', cartController.clearCart);
+router.delete('/customer/delete_cart_product', cartController.removeProductInCart)
+
+module.exports = router;
