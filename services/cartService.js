341200cde69b6fc1abd83219e95a061c0e9d3070
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-26 17:27:21 +0530

    made cart services

---
+++ services/cartService.js
@@ -0,0 +1,44 @@
+const { Cart, Customer, Product } = require('../models');
+
+
+const addingProductInCart = async (body, email) => {
+
+    const { productId, quantity } = body;
+
+    const { customerId } = await Customer.findOne({ where: { email } });
+    const { price } = await Product.findOne({ where: { productId }});
+    const cartDetails = await Cart.findAll({ where: { customerId }});
+
+    const cart = await Cart.create({
+        productId,
+        customerId,
+        totalPrice: price,
+        totalQuantity: quantity,
+    });
+    const cartD = {
+        cartItems: cartDetails.length + 1,
+        ...cart.dataValues,
+    }
+    return cartD;
+}
+
+const deleteAllCartItems = async (email) => {
+
+    const { customerId } = await Customer.findOne({ where: { email } });
+    const cart = await Cart.destroy({ where: { customerId } });
+    return cart;
+}
+
+const deleteProductInCart = async (id, email) => {
+
+    const productId = id;
+    const { customerId } = await Customer.findOne({ where: { email } });
+    const cart = await Cart.destroy({ where: { customerId, productId } });
+    return cart;
+}
+
+module.exports = {
+    addingProductInCart,
+    deleteAllCartItems,
+    deleteProductInCart,
+}
