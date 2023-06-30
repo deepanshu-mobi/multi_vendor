a59846acd293af82f0fd0ca0b781cf3777aaa62c
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-06-30 13:07:55 +0530

    made cart_products

---
+++ migrations/20230629072014-create-cart-products.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cart_products', {
+      cartProductId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cartId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cart_products');
+  }
+};
