e616e279972a888783bda3ea43be8a07d0329f1c
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-07-06 18:11:32 +0530

    correct the cartProducts route

--- routes/cart.js
+++ routes/cart.js
@@ -6,6 +6,6 @@ router.use(verifyToken)
 router.post('/customer/cart', cartController.addToCart);
 router.delete('/customer/clear_cart', cartController.clearCart);
 router.delete('/customer/delete_cart_product', cartController.removeProductInCart);
-router.get('/customer/cart_proudcts', cartController.cartProducts)
+router.get('/customer/cart_products', cartController.cartProducts)
 
 module.exports = router;
