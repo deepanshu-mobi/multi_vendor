258518c7e3fb5d705a98d20dea78203571642a30
Deepanshusingh01 <deepanshu@mobikasa.com>
2023-07-06 13:26:00 +0530

    made changes in order migration

--- migrations/20230601062724-create-order.js
+++ migrations/20230601062724-create-order.js
@@ -26,23 +26,8 @@ module.exports = {
         type: Sequelize.ENUM(constant.OrderStatus.PENDING, constant.OrderStatus.APPROVED, constant.OrderStatus.CANCELLED, constant.OrderStatus.FAILED),
         defaultValue: constant.OrderStatus.PENDING
       },
-      shippingAddressCity: {
-        type: Sequelize.STRING
-      },
-      shippingAddressCountry: {
-        type: Sequelize.STRING
-      },
-      shippingAddressLine1: {
-        type: Sequelize.STRING
-      },
-      shippingAddressLine2: {
-        type: Sequelize.STRING
-      },
-      shippingAddressPostalCode: {
-        type: Sequelize.STRING
-      },
-      shippingAddressState: {
-        type: Sequelize.STRING
+      customerLocationId: {
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
